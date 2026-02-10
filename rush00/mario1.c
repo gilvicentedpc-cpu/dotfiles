1399c09da2369d321e9d82b61b1a2ebd021a3aea
gilvicentedpc-cpu <252907896+gilvicentedpc-cpu@users.noreply.github.com>
2026-02-10 03:06:44 -0300

    update

---
+++ rush00/mario1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+    int altura, linha, coluna, espaco;
+     
+    do{
+        altura = get_int("altura");
+        }
+        while(altura < 1 || altura > 8);
+
+        for (linha = 0; linha < altura; linha++){
+
+            for (espaco = 0; espaco < (altura - linha -1); espaco++){
+                printf(" ");
+                 }
+
+            for(coluna = 0; coluna <= linha; coluna++){
+                printf("0");
+            }
+            printf("\n");
+         }
+}
